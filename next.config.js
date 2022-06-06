9607367ec1ff877b5f582e75c213d62af5e13baf
Alireza Askarpour <askarpourdev@gmail.com>
2022-06-06 13:22:12 +0430

    feat: implement brightness api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  webpack5: true,
+  env: {
+    API_URL: "http://localhost:3000/api",
+  },
 }
-
-module.exports = nextConfig
