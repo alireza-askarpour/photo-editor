51a139015a05a44a3ef03eb1108b53124be118ce
Alireza Askarpour <askarpourdev@gmail.com>
2022-07-14 12:43:17 +0430

    faet: add front poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "Arial", "Helvetica", "sans-serif"],
+    },
     extend: {},
+    colors: {
+      ...colors,
+    }
   },
   plugins: [],
 }
